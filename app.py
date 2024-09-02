b64f3a11e479d72df5057af03d5fe217ab4bdae9
trailtony <still_running_uphill@protonmail.com>
2024-09-02 15:21:13 +0100

    Added PLACEHOLDER_CODE as constant to app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from typing import Final
 
 app = Flask(__name__)
 
+# PLACEHOLDER_CODE defined as a constant due to it's use throughout the project
+PLACEHOLDER_CODE: [Final] = "print('Hello, World!')" 
 
 @app.route("/", methods=["GET"])
 def code():
     context = {
         "message": "Paste Your Python Code 🐍",
+        "code": PLACEHOLDER_CODE
     }
     return render_template("code_input.html", **context)
