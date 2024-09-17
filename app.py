ec71b02ea9021891e829f71e330a56e1884201de
trailtony <still_running_uphill@protonmail.com>
2024-09-17 19:40:29 +0100

    Added screenshot feature to app.py and styled the code image template

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import (
-    Flask,
-    redirect, 
-    render_template,
-    request, 
-    session,
-    url_for
-)
+import base64
+from flask import Flask, redirect, render_template, request, session, url_for
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import Python3Lexer
 from pygments.styles import get_all_styles
+from typing import Dict, Text
 
-from typing import Dict, Text, Response
-from utils.generate_app_secret_key import create_flask_secret_key
+from utils import take_screenshot_from_url
+from session_auth.generate_app_secret_key import create_flask_secret_key
+
 
 app = Flask(__name__)
 app.secret_key = create_flask_secret_key()
-#app.config["TESTING"] = True
+# app.config["TESTING"] = True
 
 # PLACEHOLDER_CODE defined as a constant due to it's use throughout the project
 PLACEHOLDER_CODE = "print('Hello, World!')"
 # Starting Pygments style name
 DEFAULT_STYLE = "default"
+
 
 @app.route("/", methods=["GET"])
 def code() -> Text:
@@ -32,51 +29,67 @@ def code() -> Text:
         "message": "Paste Your Python Code 🐍",
         "code": session["code"],
         "num_lines": len(lines),
-        "max_chars": len(max(lines, key=len))
+        "max_chars": len(max(lines, key=len)),
     }
     return render_template("code_input.html", **context)
 
+
 # View to reset a user’s session
 @app.route("/save_code", methods=["POST"])
-def save_code() -> Response:
+def save_code():
     session["code"] = request.form.get("code")
     return redirect(url_for("code"))
 
+
 # View to save a user’s custom code in session
 @app.route("/reset_session", methods=["POST"])
-def reset_session() -> Response:
+def reset_session():
     session.clear()
     session["code"] = PLACEHOLDER_CODE
     return redirect(url_for("code"))
+
 
 # View to set Pygments definitions and render a template that shows the highlighted code
 @app.route("/style", methods=["GET"])
 def style() -> Text:
     if session.get("style") is None:
         session["style"] = DEFAULT_STYLE
-    formatter = HtmlFormatter(style = session["style"])
+    formatter = HtmlFormatter(style=session["style"])
     context: Dict = {
         "message": "Select Your Style 🎨",
         "all_styles": list(get_all_styles()),
         "style_definitions": formatter.get_style_defs(),
         "style_bg_color": formatter.style.background_color,
-        "highlighted_code": highlight(session["code"], Python3Lexer(), formatter)
+        "highlighted_code": highlight(session["code"], Python3Lexer(), formatter),
     }
     return render_template("style_selection.html", **context)
 
+
 # View that receives the "style" value from the posted form of style_selection.html
 @app.route("/save_style", methods=["POST"])
-def save_style() -> Response:
+def save_style():
     if request.form.get("style") is not None:
         session["style"] = request.form.get("style")
     if request.form.get("code") is not None:
         session["code"] = request.form.get("code")
     return redirect(url_for("style"))
 
+
 # View to load and populate image.html with a success message
 @app.route("/image", methods=["GET"])
-def image() -> Text:
-    context = {
+def image():
+    session_data: Dict = {
+        "name": app.config["SESSION_COOKIE_NAME"],
+        "value": request.cookies.get(app.config["SESSION_COOKIE_NAME"]),
+        "url": request.host_url,
+    }
+
+    target_url = request.host_url + url_for("style")
+    image_bytes = take_screenshot_from_url(target_url, session_data)
+
+    context: Dict = {
         "message": "Done! 🎉",
+        # Base64-encode the binary image data and convert it to a Base64 string
+        "image_b64": base64.b64encode(image_bytes).decode("utf-8"),
     }
     return render_template("image.html", **context)
--- utils.py
+++ utils.py
@@ -1,19 +1,25 @@
-import typing
 from playwright.sync_api import sync_playwright
 
-target_url = "https://www.example.com"
-element = "h1"
 
-with sync_playwright() as playwright:
-    webkit = playwright.webkit
-    browser = webkit.launch()
-    browser_context = browser.new_context()
-    page = browser_context.new_page()
-    page.goto(target_url)
-    found_element = page.locator(element)
-    text = found_element.text_content()
-    print(text)
-    browser.close()
-
-# <Response url='https://www.example.com/' request=>
-# 'Example Domain'
+def take_screenshot_from_url(url, session_data):
+    # Context manager to wrap the Playwright code
+    with sync_playwright() as playwright:
+        # create and launch a headless WebKit browser
+        webkit = playwright.webkit
+        browser = webkit.launch()
+        # new context for the browser named browser_context.
+        # Set the device_scale_factor to 2. Increasing the scale factor
+        # will make sure that the picture you take isn’t pixelated.
+        browser_context = browser.new_context(device_scale_factor=2)
+        # adds the session info to browser_context with .add_cookies()
+        browser_context.add_cookies([session_data])
+        # opens a new browser window in your browser_context.
+        page = browser_context.new_page()
+        # visits the target URL
+        page.goto(url)
+        # takes a screenshot of the element with a code class.
+        screenshot_bytes = page.locator(".code").screenshot()
+        # closes the browser
+        browser.close()
+        # returns the image buffer
+        return screenshot_bytes
